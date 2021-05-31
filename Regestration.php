ca8f00f90ab47be35ffa0df43841b8f20f720590
shahriyar787 <85014351+shahriyar787@users.noreply.github.com>
2021-05-31 20:20:19 +0600

    REGISTRATION  FROM
    Registration From code

---
+++ Regestration.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>REGISTRATION</title>
+	<h1>Registration Form</h1>
+  </head>
+  <body>
+  <form>   
+    <table border="0">
+
+      <tr>       
+        <td>Full Name:</td>
+		<td></td>
+		<td></td>
+        <td><input name="Usename" type="text"></td>		
+      </tr>
+
+      <tr>      
+        <td>E-mail:</td>
+		<td></td>
+		<td></td>
+        <td><input name="mail" type="text"></td>
+      </tr>	  
+
+	   <tr>       
+        <td>Password:</td>
+		<td></td>
+		<td></td>
+        <td><input name="pass" type="password"></td>
+      </tr>	
+
+	  <tr>      
+        <td>Comment:</td>
+		<td></td>
+		<td></td>
+        <td><textarea name="nam" id="10" cols="30" rows="4"></textarea></td>
+      </tr>
+
+	  <tr>        
+        <td>Gender:</td>
+		<td></td>
+		<td></td>
+		<td><input type="radio" name="gender">Female
+            <input type="radio" name="gender">Male
+            <input type="radio" name="gender">Other
+            </td>
+      </tr>	
+
+	   <tr>       
+        <td>Please Choose a hobby</td>
+		<td></td>
+		<td></td>
+        <td><input type="checkbox">Singing
+            <input type="checkbox">Dancing
+            <input type="checkbox">Reading
+            </td>
+      </tr>	  
+
+	  <tr>       
+        <td>Please Choose a file</td>
+		<td></td>
+		<td></td>
+        <td> <input type="file"</td>
+      </tr>
+
+    </table>	
+	  <input type="submit" value="Submit">
+	  <input type="reset">
+  </form>
+  </body>
+</html>
